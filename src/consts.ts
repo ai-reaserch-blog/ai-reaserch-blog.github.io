aca75470c3bc0d7951ebc38787477266bc811594
WangRongsheng <fujingshusheng@gmail.com>
2025-01-17 15:55:10 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,19 +2,19 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Base Page Metadata, src/layouts/BaseLayout.astro
-export const BRAND_NAME = "NovaSky";
-export const SITE_TITLE = "NovaSky";
-export const SITE_DESCRIPTION = "Next-generation Open Vision and AI @ Berkeley Sky Computing Lab";
+export const BRAND_NAME = "Blog";
+export const SITE_TITLE = "Blog";
+export const SITE_DESCRIPTION = "Blog @ RongshengWang";
 
 // Tags Page Metadata, src/pages/tags/index.astro
-export const Tags_TITLE = "NovaSky - All Tags";
+export const Tags_TITLE = "Blog - All Tags";
 export const Tags_DESCRIPTION =
-  "NovaSky - All tags and the count of articles related to each tag";
+  "Blog - All tags and the count of articles related to each tag";
 
 // Tags Page Metadata, src/pages/tags/[tag]/[page].astro
 export function getTagMetadata(tag: string) {
   return {
-    title: `All articles on '${tag}' tag in NovaSky`,
+    title: `All articles on '${tag}' tag in Blog`,
     description: `Explore articles about ${tag} for different perspectives and in-depth analysis.`,
   };
 }
@@ -22,32 +22,32 @@ export function getTagMetadata(tag: string) {
 // Category Page Metadata, src/pages/category/[category]/[page].astro
 export function getCategoryMetadata(category: string) {
   return {
-    title: `All articles in '${category}' category in NovaSky`,
-    description: `Browse all articles under the ${category} category in NovaSky`,
+    title: `All articles in '${category}' category in Blog`,
+    description: `Browse all articles under the ${category} category in Blog`,
   };
 }
 
 // Header Links, src/components/Header.astro
 export const HeaderLinks = [
-  { href: "/posts/about-us/", title: "About Us" },
+  // { href: "/posts/about-us/", title: "About Us" },
   { href: "/category/One/1/", title: "Blog Posts" },
   { href: "/tags/", title: "Sort by Tags" },
 ];
 
 // Footer Links, src/components/Footer.astro
 export const FooterLinks = [
-  { href: "https://sky.cs.berkeley.edu/", title: "Sky Computing Lab @ Berkeley" },
+  { href: "https://www.wangrs.site/", title: "Copyright @ RongshengWang" },
 ];
 
 // Social Links, src/components/Footer.astro
 export const SocialLinks = [
   {
-    href: "https://x.com/NovaSkyAI",
+    href: "https://www.kaggle.com/rongshengwang",
     icon: "tabler:brand-twitter",
     label: "Twitter",
   },
   {
-    href: "https://github.com/NovaSky-AI",
+    href: "https://github.com/WangRongsheng",
     icon: "tabler:brand-github",
     label: "GitHub",
   },
